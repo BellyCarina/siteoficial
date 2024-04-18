5ab438aefcc5f71fbbd25657e4b0abc08aa1e505
BellyCarina <164196921+BellyCarina@users.noreply.github.com>
2024-04-18 18:26:50 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel img');
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+showImage(currentIndex);
+setInterval(nextImage, 5000); // Troca de imagem a cada 5 segundos
